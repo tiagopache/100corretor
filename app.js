9a065b6b230c4a70612111a6b7e86561315cfe21
Tiago Pache de Faria <tiagopache@gmail.com>
2017-06-13 22:27:06 -0300

    add application insight tracking

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const appInsights = require('applicationinsights');
+
+appInsights.setup()
+            .setAutoDependencyCorrelation(true)
+            .setAutoCollectRequests(true)
+            .setAutoCollectPerformance(true)
+            .setAutoCollectExceptions(true)
+            .setAutoCollectDependencies(true)
+            .start();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
